332f216277c0cad65bed883b1118d14dc065ff2f
pkdhanushmca <pkdhanushmca@gmail.com>
2026-07-14 11:41:41 +0530

    Add component example

---
+++ src/Table.jsx
@@ -0,0 +1,32 @@
+export default function Table() {
+    return (
+        <>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>EMPLOYEE NAME</th>
+                    <th>DESIGNATION</th>
+                    <th>SALARY</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Dhanush</td>
+                    <td>Java Full Stack Developer</td>
+                    <td>50,000</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Sathish</td>
+                    <td>Python Full Stack Developer</td>
+                    <td>60,000</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Subash</td>
+                    <td>Mern Full Stack Developer</td>
+                    <td>50,000</td>
+                </tr>
+            </table>
+        </>
+    );
+}
